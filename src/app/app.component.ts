28c2bdbdb564e5b3eb1a672a91678534d5e3e54d
deepak-ramesh <deepakramesh.ai@gmail.com>
2024-12-20 20:24:29 +0000

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,CommonModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'map-task';
+  title = 'map-test';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,3 @@
 import { Routes } from '@angular/router';
-import { MapComponent } from './map/map.component';
 
-export const routes: Routes = [
-    { path: '', redirectTo: 'map', pathMatch: 'full' }, // Redirect root to the map
-    { path: 'map', component: MapComponent },
-];
-
+export const routes: Routes = [];
