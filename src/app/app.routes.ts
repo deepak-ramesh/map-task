bed318235cb49cecd4aa1d20207d04bd92d3233d
deepak-ramesh <deepakramesh.ai@gmail.com>
2024-12-20 21:00:34 +0000

    new changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { MapComponent } from './map/map.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'map', pathMatch: 'full' }, // Redirect root to the map
+    { path: 'map', component: MapComponent },
+];
+
+
+
